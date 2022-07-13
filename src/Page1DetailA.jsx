6970f3a7152cdf8fdce6b51a65dd7dbbc517709d
mh-kurihara <108643305+mh-kurihara@users.noreply.github.com>
2022-07-13 17:42:40 +0900

    更新

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailA = () => {
+  return (
+    <div>
+      <h1>Page1詳細Aページです</h1>
+    </div>
+  );
+};
---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>Page1詳細Bページです</h1>
+    </div>
+  );
+};
